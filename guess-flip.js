b622901d96c4fc76e7c19c2ddc658e0843a3b91a
@sep0225 <sep0225@live.unc.edu>
2022-04-22 14:24:51 -0400

    guess-flip.js

---
+++ guess-flip.js
@@ -0,0 +1,12 @@
+import { flipACoin } from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = creteRequire(import.meta.url);
+const args = (require("./node_modules/minimist"))(process.argv.slice(2))
+args["call"]
+const call = args.call || process.env.CALL || 0
+if ((call == 0) || ((call != "tails") && (call != "heads"))) {
+    console.log("Error: no input.");
+    console.log("Usage: node guess-flip --call=[heads|tails}");
+} else {
+    console.log(flipACoin(call));
+}
