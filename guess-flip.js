3d2f206ecaab1c811f056f3b2e16ab0c66527204
@sep0225 <sep0225@live.unc.edu>
2022-04-23 12:26:04 -0400

    fix guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -1,12 +1,22 @@
+// import { flipACoin } from "./modules/coin.mjs";
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const args = (require("./node_modules/minimist"))(process.argv.slice(2))
+// args["call"]
+// const call = args.call || process.env.CALL || 0
+// if ((call == 0) || ((call != "tails") && (call != "heads"))) {
+//     console.log("Error: no input.");
+//     console.log("Usage: node guess-flip --call=[heads|tails}");
+// } else {
+//     console.log(flipACoin(call));
+// }
+
 import { flipACoin } from "./modules/coin.mjs";
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const args = (require("./node_modules/minimist"))(process.argv.slice(2))
-args["call"]
-const call = args.call || process.env.CALL || 0
-if ((call == 0) || ((call != "tails") && (call != "heads"))) {
-    console.log("Error: no input.");
-    console.log("Usage: node guess-flip --call=[heads|tails}");
+import minimist from 'minimist'
+const arg = minimist(process.argv.slice(2))
+if (arg.call = 'heads' || arg.call == 'tails') {
+    console.log(flipACoin(arg.call))
 } else {
-    console.log(flipACoin(call));
+    console.log("Error: no input.")
+    console.log("Usage: node gues-flip --call=[heads|tails}")
 }
